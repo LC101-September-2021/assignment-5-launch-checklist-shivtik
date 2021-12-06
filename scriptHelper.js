7e3ae4ae589462d925e50ce306912c6a14a9942f
Shivani <shivani>
2021-12-05 18:49:05 -0600

    Fix innerHTML element

--- scriptHelper.js
+++ scriptHelper.js
@@ -15,7 +15,7 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
                 <img src="">
    */
   let mt = document.getElementById("missionTarget")
-  mt.innerHTML += `<!--Fetchsomeplanetarydata--><h2>Mission Destination</h2><ol><li>Name:${name} </li><li>Diameter: ${diameter} </li><li>Star: ${star}</li><li>Distance from Earth: ${distance} </li><li>Number of Moons: ${moons}</li></ol><img src=${imageUrl}>`
+  mt.innerHTML = `<h2>Mission Destination</h2><ol><li>Name:${name} </li><li>Diameter: ${diameter} </li><li>Star: ${star}</li><li>Distance from Earth: ${distance} </li><li>Number of Moons: ${moons}</li></ol><img src=${imageUrl}>`
                    
 }
 
